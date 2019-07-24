0ce771e66445c55a6eaab8fcc4ee0a9461c32e5a
gunt <4261629+gunt@users.noreply.github.com>
2019-07-24 22:20:13 +0200

    3 Objects (Pokemon) to the Array added

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,36 @@
 
 //  1. Everything between the curly braces makes up the object.
 //  2. variable called repository and assign it to be an array.
-//  3. Several objects to the array 
+//  3. Several objects to the array - Each object represents one 
+//  Pokémon and needs to have the same keys to avoid errors. 
+//  three keys to each pokemon. name, height, types
+//  Create at least three Pokémon objects in your repository array.
+//  Object: {}
+//  Array:  []
 
 var repository = [
+
+    {
+        name: "Talonflame",
+        height: 3.11,
+        types: ['fire', 'flying']  
+    },
+
+    {
+        name: "Buzzwole",
+        height: 7.10,
+        types: ['bug', 'fighting']  
+    },
+
+    {
+        name: "Entei",
+        height: 6.11,
+        types: ['fire']  
+    },
+
+
+
+
+
 
 ]
