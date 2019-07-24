1b424d0b71acfaddf9e307b22333ddd25d2e6690
gunt <4261629+gunt@users.noreply.github.com>
2019-07-24 21:04:40 +0200

    creating an Array - variable Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-alert('Hello world');
+// var mixedArray = [
+//     1,
+//     'two',
+//     numberArray,
+//     { age: 5 }
+//   ];
+
+//  1. Everything between the curly braces makes up the object.
+//  2. variable called repository and assign it to be an array.
+//  3. Several objects to the array 
+
+var repository = [
+
+]
