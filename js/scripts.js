9186b8d32d845fa044c66c41e7ba61faddb16bca
gunt <4261629+gunt@users.noreply.github.com>
2019-07-24 23:52:53 +0200

    adding space in <p>

--- js/scripts.js
+++ js/scripts.js
@@ -56,11 +56,12 @@ var repository = [
 
 for (var i = 0; i < repository.length; i++) {
 
-    var pokemon = repository[i];
+    var pokemon = repository[i]; 
+    
 
     // document.write("pokemon.name:" , i, repository[i])
 
-    if (repository[i].height === 7)  {
+    if (repository[i].height > 7)  {
         document.write ('<p>' + pokemon.name + "Wow, that’s big!" + pokemon.height + '<p>');
     }   else {
         document.write ('<p>' + pokemon.name + pokemon.height + '<p>')
