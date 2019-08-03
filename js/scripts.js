84d03aa2d6ea8ad432e98893808b16f12fe5d7b5
gunt <4261629+gunt@users.noreply.github.com>
2019-08-04 00:14:07 +0200

    update function property according task pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
-
-
 // var pokemonRepository = (function () {
 //     var repository = [];
-  
+
 //     function add(pokemon) {
 //       repository.push(pokemon);
 //     }
-  
+
 //     function getAll() {
 //       return repository;
 //     }
-  
+
 //     return {
 //       add: add,
 //       getAll: getAll
@@ -20,47 +18,58 @@
 // creating before anything else a new pokemonRepository variable
 //Step 2: is Inside no need to do it again. 
 var pokemonRepository = (function () {
-var repository = [
+    var repository = [
 
-    {
-        name: "Talonflame",
-        height: 3.11,
-        types: ['fire', 'flying']
-    },
+        {
+            name: "Talonflame",
+            height: 3.11,
+            types: ['fire', 'flying']
+        },
 
-    {
-        name: "Buzzwole",
-        height: 7.10,
-        types: ['bug', 'fighting']
-    },
+        {
+            name: "Buzzwole",
+            height: 7.10,
+            types: ['bug', 'fighting']
+        },
 
-    {
-        name: "Entei",
-        height: 6.11,
-        types: ['fire']
-    },
+        {
+            name: "Entei",
+            height: 6.11,
+            types: ['fire']
+        },
 
-];
+    ];
 
-// recode - wrap your code to a IIFE // the same but with the clean code // last paragraph
-// organize the code according to task
+    // recode - wrap your code to a IIFE // the same but with the clean code // last paragraph
+    // organize the code according to task
 
-function add(pokemon) {
-          repository.push(pokemon);
-        }
+    function add(pokemon) {
+        repository.push(pokemon);
+    }
 
-function getAll() {
-    return repository;
-}
+    function getAll() {
+        return repository;
+    }
 
-return {
-          add: add,
-          getAll: getAll
-        };
-      })();
-    
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+
+pokemonRepository.add({
+    name: 'Pikachu',
+    height: 1.4,
+    types: ['electric']
+});
 
 //Part 2: forEach Loops // 
-repository.forEach(function (property) {
-    document.write('<p> ' + property.name + ':' + '  ' + property.height + ' - ' + property.types + '</p>');
+// console.log(pokemonRepository.getAll()); instead
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height > 7) {
+        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') Wow, that\'s big!' + '</p>');
+    } else {
+        document.write('<p> ' + pokemon.name + ':' + '  ' + pokemon.height + ' - ' + pokemon.types + '</p>');
+    }
 });
