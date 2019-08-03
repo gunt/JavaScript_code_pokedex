33f991b61e0e8dd777d1a644c54f589b7790e66e
gunt <4261629+gunt@users.noreply.github.com>
2019-08-04 01:02:21 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -68,7 +68,7 @@ pokemonRepository.add({
 // console.log(pokemonRepository.getAll()); instead
 pokemonRepository.getAll().forEach(function (pokemon) {
     if (pokemon.height > 7) {
-        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') Wow, that\'s big!' + '</p>');
+        document.write('<p id="pokeH-style"> ' + pokemon.name + ' (height: ' + pokemon.height + ') Wow, that\'s big!' + '</p>');
     } else {
         document.write('<p> ' + pokemon.name + ':' + '  ' + pokemon.height + ' - ' + pokemon.types + '</p>');
     }
