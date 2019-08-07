0eb9363205038d20220e699a59124d90fd208327
gunt <4261629+gunt@users.noreply.github.com>
2019-08-06 18:59:14 -0600

    clean coding organize

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,9 @@ var pokemonRepository = (function () {
 
     ];
 
+    function getAll() {
+        return repository;
+    }
 
     // recode - wrap your code to a IIFE // the same but with the clean code // last paragraph
     // organize the code according to task
@@ -39,52 +42,48 @@ var pokemonRepository = (function () {
 
     }
 
-    function getAll() {
-        return repository;
+    function addListItem(pokemon) {
+
+        //creating the button // &textButton  // var $pokebuttonText = document.createTextNode(name);
+
+        var $listItemElement = document.createElement('li');
+        var $pokebutton = document.createElement('button');
+        var $pokelist = document.querySelector('.pokemon-list');
+
+        //$element.classList.add('my-class'); // Adds the class if it isn't present yet
+        //..specifically with classes, you shouldn’t use a dot in front of the class
+        $listItemElement.setAttribute('class', 'pokemon-list__item')
+        $pokebutton.setAttribute('class', 'button') // testing the class button default
+
+        //appendchild
+        $listItemElement.appendChild($pokebutton);
+        $pokelist.appendChild($listItemElement);
+
+        $pokebutton.innerText = pokemon.name;
+        $pokebutton.addEventListener('click', function (event) {
+            showDetails(event.target.innerText);
+        });
+
     }
+
+    // show details function // console log // 
+    function showDetails(pokemon) {
+        // var name = pokemon.name;
+        console.log(pokemon);
+    }
+
+
 
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
     };
 })();
 
-pokemonRepository.add({
-    name: 'Pikachu',
-    height: 1.4,
-    types: ['electric']
+var Pokemons = pokemonRepository.getAll();
+
+Pokemons.forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
-
-
-var $pokemonList = document.querySelector('.pokemon-list');
-
-
-function addListItem(pokemon, $pokemonContainer)
-
-var name = pokemon;
-
-var $listItem = document.createElement('li');
-
-//$element.classList.add('my-class'); // Adds the class if it isn't present yet
-//..specifically with classes, you shouldn’t use a dot in front of the class
-$listItem.classList.add('pokemonItem');
-
-//creating the button // &textButton
-var $button = document.createElement('button');
-var $buttonText = document.createTextNode(name);
-//append - button - text
-$button.appendChild($buttonText);
-// var $container = document.querySelector('.container');
-// var button = document.createElement('button');
-// button.innerText = "Click Me";
-// $container.appendChild(button);
-$
-
-
-// //Part 2: forEach Loops // 
-// // console.log(pokemonRepository.getAll()); instead
-// pokemonRepository.getAll().forEach(function (pokemon) {
-    
-    
-
-// });
